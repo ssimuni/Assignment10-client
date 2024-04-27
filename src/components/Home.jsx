a36405b7856cb2984c9ce2bba91e396e8ae6fddc
unknown <ssimuni>
2024-04-27 16:19:46 +0600

    card done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import { useLoaderData } from 'react-router-dom'
+import ArtCard from './ArtCard';
 
 const Home = () => {
     const paintings = useLoaderData();
     return (
-        <div>data: {paintings.length}</div>
+        <div>
+            data: {paintings.length}
+            <div className='flex justify-around'>
+                <div className='lg:grid lg:grid-cols-2 gap-20'>
+                    {
+                        paintings.map(painting => <ArtCard
+                            key={painting._id}
+                            painting={painting}
+                        ></ArtCard>)
+                    }
+                </div>
+            </div>
+        </div>
     )
 }
 
